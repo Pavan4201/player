26161bfc167534e90afe86037f9dc855c516517d
pavan <mylipilli.srisa@hcltech.com>
2026-03-08 11:53:35 +0530

    third

--- src/test/java/Abcd/Abcde/Proper.java
+++ src/test/java/Abcd/Abcde/Proper.java
@@ -3,7 +3,8 @@ package Abcd.Abcde;
 public class Proper {
 
 	public static void main(String[] args) {
-		System.out.println("pavan");
+		System.out.println("pavan kumar");
+		System.out.println("sai");
 
 	}
 
